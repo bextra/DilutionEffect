5e3866963b2eaa65022dfb77a92681e922406ebe
KristenBeck <kristenbeck527@gmail.com>
2013-01-22 12:08:50 -0800

    Separated out file reading into its own subroutine. Cleaned up adjust_abundance code to read more clear especially when totalling high and low abundance genes.

--- dilution_effect.pl
+++ dilution_effect.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 # dilution_effect.pl
-# by K_Beck
+# K. Beck
 use strict; use warnings;
 use Getopt::Std;
 use vars qw ($opt_h $opt_v $opt_a $opt_d);
@@ -31,30 +31,35 @@ print "Usage: dilution_effect.pl [options] <arguments...>
 my ($filename) = @ARGV; # renames ARGV
 
 # Define global variables
+my $header;
 my $total_freq;
 my %genes_to_frequency; # gene ids and their frequencies
 my %genes_to_abundance; # gene ids and their abundance as a proportion
 						# of total frequency of all genes
 
-# THRESHOLD CHECKS:
+# THRESHOLD QUALITY CONTROL CHECKS:
+my $threshold;
 if ($opt_a) {
 	die "Usage: $0 -a <threshold 0.0 to 1.0> <file>\n" unless (($opt_a > 0) and ($opt_a <=1));
 	# Need one floating point number between 0 and 1 as command line argument 
+	$threshold = $opt_a; # Renames threshold for easy calling
+	print STDERR "The specified threshold is $threshold\n";
 }
 
-exit; 
+# Determine threshold if option selected
+if ($opt_d) {
+	$threshold = determine_threshold();
+	print STDERR" The determined threshold is $threshold\n";
+}
 
-my $threshold = $opt_a; #renames threshold for easy calling
-print STDERR "The threshold is $threshold\n";
+# Get data from file (ref to hash) and cumulative frequency across all genes
+load_data($filename);
+printf STDERR "The summative raw frequency of all genes is %.4f\n", $total_freq;
 
-#calls sub to adjust frequency of low abundance transcripts
-
+# Adjust frequency of low abundance transcripts
 adjust_abundance() if ($opt_a);
 
-determine_threshold() if ($opt_d);
-
 warn "Other arguments were: @ARGV\n\n";
-
 
 
 # # # # # # # # # # #
@@ -62,37 +67,32 @@ warn "Other arguments were: @ARGV\n\n";
 # SUBROUTINES
 #
 # # # # # # # # # # #
-
-sub adjust_abundance {
-	my $header = <>;
-	#print "This is the header $header\n"; # checkpoint
+sub load_data {
+	$header = <>;
 	
-	#store frequencies and find frequency total
+	# Store frequencies and find frequency total
 	while (my $line = <>) {
-		next if ($line =~ m/^tracking_id/i); #skips first line
+		next if ($line =~ m/^tracking_id|GeneID/i); # Skips first line
 		
-		#store gene IDs and freq to hash
+		# Store gene IDs and freq in a hash
 		my ($gene, $raw_freq) = split(/\s+/, $line);
-		#print "$raw_freq\n"; #checkpoint
+
 		$genes_to_frequency{$gene} = $raw_freq;
 		
 		#takes the total of all frequencies
 		$total_freq += $raw_freq;
 	}
-	printf STDERR "The summative raw frequency of all genes is %.4f\n", $total_freq;
-	
-	#calculate threshold percent from total and store as threshold index
-	#for empirical threshold, determine the desired index and relative threshold
+}
+
+sub adjust_abundance {
+	## Use threshold to determine threshold index
 	my $threshold_index = $total_freq * $threshold;
 	printf STDERR "The threshold index is %.4f\n", $threshold_index;
-	#printf STDOUT "The threshold index is %.4f\n", $threshold_index;
 	
-	#find the sum of high abundance genes
-	my $high_abundance_total = 0;
-	my $low_abundance_total = 0;
-	
-	# my ($high_abundance_total, $low_abundance_total) = (0, 0);
+	# Init variables for expression class totals
+	my ($high_abundance_total, $low_abundance_total) = (0, 0);
 
+	## Sum counts from low and high abundance genes separately	
 	while (my ($gene, $raw_freq) = each %genes_to_frequency) {
 		if ($raw_freq >= $threshold_index) {
 			$high_abundance_total += $raw_freq;
@@ -100,29 +100,30 @@ sub adjust_abundance {
 			$low_abundance_total += $raw_freq;
 		}
 	}
-	
 	printf STDERR "The total expression in low abundance is %.4f\n", $low_abundance_total;
-	
-	if ($high_abundance_total == 0) {
-		# will most likely only be pre-puberty set
-		print "GeneID\tRaw Expression\n";
-		while (my ($gene, $raw_freq) = each %genes_to_frequency) {
-			print "$gene $genes_to_frequency{$gene}\n"
-		}
-		die "No values are above set threshold\n";
-	} else { 
-	printf STDERR "The total expression in high abundance is %.4f\n", $high_abundance_total;
+	if ($high_abundance_total > 0) { 
+		printf STDERR "The total expression in high abundance is %.4f\n", $high_abundance_total;
 	}
-#calculate dilution proportion and adjustment factor
+	
+	## Return data if no genes in high abundance class
+	if ($high_abundance_total == 0) {
+		# Most likely with non-lactating or control data
+		print "GeneID\tRawExpression\n";
+		while (my ($gene, $raw_freq) = each %genes_to_frequency) {
+			print "$gene\t$genes_to_frequency{$gene}\n"
+		}
+		die "No values are above set threshold: $threshold\n";
+	} 
+	
+	## Calculate dilution proportion and adjustment factor
 	my $dilution_proportion = $high_abundance_total / $total_freq;
 	my $adj_factor = 1/(1-$dilution_proportion);
 	printf STDERR "The dilution proportion is %.6f\n", $dilution_proportion;
 	printf STDERR "The adjustment factor is %.6f\n", $adj_factor; 
-	#printf STDOUT "The dilution factor is %.6f\n", $dilution_factor;
 
 
 
-#apply dilution factor to genes less than threshold index	
+	## Apply dilution factor to genes less than threshold index	
 	my $dil_adjusted_freq;
 	print STDOUT "GeneID\tRawExp\tAdjExp\n";
 
@@ -144,4 +145,8 @@ sub adjust_abundance {
 	print STDERR "There were $highgenes high abundance genes\n";	
 }
 
+sub determine_threshold {
+	return(5);
+
+}
 __END__
