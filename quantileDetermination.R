0af167f5366c31e3aa7acb75adf19088b800664c
KristenBeck <kristenbeck527@gmail.com>
2013-01-23 15:39:52 -0800

    Included notes on determining which function to use when analyzing your distribution

--- quantileDetermination.R
+++ quantileDetermination.R
@@ -19,15 +19,43 @@ wilcox.test(x=prepuberty$mean, y=lactation$mean)
 # therefore colostrum and mature are nonidentical populations
 # ref: http://www.r-tutor.com/elementary-statistics/non-parametric-methods/mann-whitney-wilcoxon-test
 
+# This needs m and n values, but how do you determine those?
 plot(dwilcox(lactation$mean))
-?dwilcox
+
 ## Kolmogorov-Smirnov Tests
 ks.test(x=prepuberty$mean, y=lactation$mean)
 ks.test(x=NRcolostrum$mean, y=NRmature$mean)
 
 
-
 ## Explore Pareto functions
+library("VGAM")
+?qpareto
 
+# Description of statistical modeling
+# d = probability density values (pdf = probability distribution function)
+    # returns height of the distribution curve at specified (input) x value(s)
+    # in some functions you can specify the mean and sd to tailor it to your curve
+    # otherwise it treats x as the z score
+# p = cumulative probabilities (cdf = cumulative distribution function)
+    # returns the area below the curve up until the value specified
+    # i.e. pnorm(1) returns the cumulative probabilities up until 1 in the curve specified
+    # defaults to mean = 0 and sd = 1
+    # to find the area above that point pnorm(1, lower.tail=FALSE)
+# q = quantile values
+    # gives you quantiles or critical values
+    # qnorm(0.95) # p = 0.05, one-tailed (upper) will return 1.644854
+    # qnorm(0.95, mean = 5, sd =1) # can shift the curve to fit your data like so
+# r = random numbers from the distribution
+# ref: http://ww2.coastal.edu/kingw/statistics/R-tutorials/prob.html
 
+# returns the x value at this portion of the curve
+quantile(rivers, probs=c(0.25, .5, .75))
+plot(density(rivers))
+quantile(lactation$mean, probs = c(0.05, 0.9908), names=TRUE)
+quantile(lactation$mean, probs = c(0.05, 0.95), names=TRUE)
+# ref: http://ww2.coastal.edu/kingw/statistics/R-tutorials/prob.html
 
+dpareto(lactation$mean)
+dnorm(lactation$mean)
+# ??location
+# ?? determine alpha and k values??
