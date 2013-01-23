64e4d75c66af570fd87ab29f3213c56f033de8ea
KristenBeck <kristenbeck527@gmail.com>
2013-01-23 13:17:16 -0800

    Comment and clean up statistics and plots to explore raw expression counts. Break off Wilcoxon-Whitney tests into independent script.

--- exploreRawCounts.R
+++ exploreRawCounts.R
@@ -4,57 +4,49 @@
 
 # Load data with exprCountsLoad.R
 options(scipen= 10)
-plot(density(NRcolostrum$mean))
 
+## Plot expression data for various milk stages
+# Scatter plots
 plot(sort(NRcolostrum$mean, decreasing=FALSE))
 plot(sort(NRtransitional$mean, decreasing=FALSE))
 plot(sort(NRmature$mean, decreasing=FALSE))
+plot(NRmature$mean)
 
-hist(shortC)
+# Histograms
 hist(NRtransitional$mean)
 hist(NRmature$mean)
 
+# Density plots
+plot(density(NRcolostrum$mean))
+plot(density(log10(NRtransitional$mean)))
+plot(density(log10(NRmature$mean)))
+
+# Box plot
+boxplot(log10(NRcolostrum$mean), log10(NRtransitional$mean), log10(NRmature$mean),
+        pars = list(boxwex = 1.5, staplewex = 0.5, outwex = 0.5)
+)
+
+## Get basic descriptive statistics
+# Mean
 mean(NRcolostrum$mean)
 mean(NRtransitional$mean)
 mean(NRmature$mean)
+
+# Median
 median(NRcolostrum$mean)
 median(NRtransitional$mean)
 median(NRmature$mean)
 
+## Subset data to remove lowly abundant genes
 shortC = subset(NRcolostrum$mean, NRcolostrum$mean > 3)
 shortT = subset(NRtransitional$mean, NRtransitional$mean > 3)
 shortM = subset(NRmature$mean, NRmature$mean > 3)
 
+## Explore subsetted data
+hist(shortC)
 plot(density(shortC))
-plot(density(log10(NRtransitional$mean)))
-plot(density(log10(NRmature$mean)))
-boxplot(log10(NRcolostrum$mean), log10(NRtransitional$mean), log10(NRmature$mean),
-        pars = list(boxwex = 1.5, staplewex = 0.5, outwex = 0.5)
-        )
-?boxplot
-plot(NRmature$mean)
-mean(NRcolostrum$mean, NRtransitional$mean, NRmature$mean)
 
-###########
-# Mann-Whitney-Wilcoxon Test
-# Null hypothesis: non-lactating and lactating are identical populations
-# if p < 0.05 we can reject the null hypothesis
-
-wilcox.test(x=NRcolostrum$mean, y=NRmature$mean)
-# returns p < 2.2e-16
-# therefore non-lac and lac are nonidentical populations
-wilcox.test(x=prepuberty$mean, y=lactation$mean)
-# returns p < 2.2e-16
-# therefore colostrum and mature are nonidentical populations
-# ref: http://www.r-tutor.com/elementary-statistics/non-parametric-methods/kruskal-wallis-test
-
-
-ks.test(x=prepuberty$mean, y=lactation$mean)
-ks.test(x=NRcolostrum$mean, y=NRmature$mean)
-?ks.test
-plot(dwilcox())
-
-
+## Other descriptive plots to help determine distribution
 hist(lactation$mean, main = "Histogram  of observed data")
 plot(density(lactation$mean), main = "Density estimate of data")
 plot(ecdf(lactation$mean), main = "Empirical cumulative distribution function")
@@ -63,14 +55,11 @@ z.norm = (lactation$mean-mean(lactation$mean))/sd(lactation$mean)
 qqnorm(z.norm)
 abline(0,1)
 
-### Modeling data
-# determine the distribution model analytically using
+## Model data to determine the distribution model analytically using:
 # - mean
 # - variability
 # - skewness (type of curve)
 # - kurtosis (type of curve)
-
-install.packages("fBasics")
 library("fBasics")
 
 skewness(lactation$mean)
@@ -103,7 +92,4 @@ kurtosis(NRmature$mean)
 # Kurtosis = 3 - Mesokurtic distribution - normal distribution for example.
 # *High values indicate a high and sharp peak indicating that a few extreme differences from the mean drive an increase in variability
 # Ref: http://www.tc3.edu/instruct/sbrown/stat/shape.htm
-# 
-# 
-# 
 
