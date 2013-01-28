98d79c61e0b36ebebbebf8dadbedbab1ce920ade
KristenBeck <kristenbeck527@gmail.com>
2013-01-28 15:02:36 -0800

    Explore VGAM Pareto functions and comment on things that work and more appropriately don't work.

--- quantileDetermination.R
+++ quantileDetermination.R
@@ -27,9 +27,7 @@ ks.test(x=prepuberty$mean, y=lactation$mean)
 ks.test(x=NRcolostrum$mean, y=NRmature$mean)
 
 
-## Explore Pareto functions
-library("VGAM")
-?qpareto
+
 
 # Description of statistical modeling
 # d = probability density values (pdf = probability distribution function)
@@ -49,13 +47,43 @@ library("VGAM")
 # ref: http://ww2.coastal.edu/kingw/statistics/R-tutorials/prob.html
 
 # returns the x value at this portion of the curve
-quantile(rivers, probs=c(0.25, .5, .75))
+quantile(rivers, probs=c(0.25, .5, .75, 1))
 plot(density(rivers))
-quantile(lactation$mean, probs = c(0.05, 0.9908), names=TRUE)
+quantile(lactation$mean, probs = c(0.05, 0.95, .999908, 1), names=TRUE)
+quantile(lactation$mean, probs = c(0.05, 0.9908, .9968, 1), names=TRUE)
 quantile(lactation$mean, probs = c(0.05, 0.95), names=TRUE)
 # ref: http://ww2.coastal.edu/kingw/statistics/R-tutorials/prob.html
 
-dpareto(lactation$mean)
-dnorm(lactation$mean)
-# ??location
-# ?? determine alpha and k values??
+
+## Explore Pareto functions
+library("VGAM")
+# location and k values are set to the vignette example
+# no reason or explanation was provided in help materials
+plot(lactation$mean, dpareto(lactation$mean, location = 3, shape = exp(1)),
+     xlab = "values of x",
+     ylab = "prob. of x",
+     main = "Pareto Probability Density Split into 10 Equal Areas")
+lines(lactation$mean, dpareto(lactation$mean, location = 1, shape=.5), col = "red")
+lines(lactation$mean, dpareto(lactation$mean, location = 1, shape= .2), col = "blue")
+# Plot "works", but doesn't provide any helpful information.
+
+
+# Probability of each x value along a GPD (Generalized Pareto Distribution)
+pp = ppareto(lactation$mean, location = 3, shape = exp(1)) # How do you determine location (alpha) and shape (k) values??
+range(pp) # 0 and 1
+qq = qpareto(pp, location = alpha, shape = k) # Errors out bec. p must be between 0 & 1...conflicts with range above
+
+pareto1() # Pops out pareto formula
+# Notes: After reading through help pages, vignettes, and the manual, the documentation
+# for this package is unclear and missing crucial definitions of how to use the
+# functions.  In addition, when seeking quantiles with qpareto with an input of 
+# probabilites from ppareto() the qpareto function returns an error stating that values
+# must be between 0 and 1. However range(ppareto()) returns 0 -1.
+# For an alternative resource, explore POT package or search other GPD (Generalized Pareto
+# Distribution) functions in R.
+# plot(lactation$mean, dnorm(lactation$mean))
+
+
+### Explore Peaks Over a Threshold
+library("POT")
+demo(POT)
