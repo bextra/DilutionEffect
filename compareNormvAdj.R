fc1e9818720e8e0ccfaa658367f4caecab0f56c7
KristenBeck <kristenbeck527@gmail.com>
2013-06-05 13:02:18 -0700

    Included code to compare in humans and in cow data

--- compareNormvAdj.R
+++ compareNormvAdj.R
@@ -3,22 +3,24 @@
 # 5-31-2013
 
 
-
-# Workflow for above functions
-# BeckGeneU = read.table("~/Work/1_Milk/ProteomicsWork/Human-BeckvGao/FullDatabase/BeckAllGeneNames3.txt", 
-#                        header=TRUE, stringsAsFactors=FALSE)
-# GaoGeneU = read.table("~/Work/1_Milk/ProteomicsWork/ProteinLists-Other's/GaoAllGeneList.txt", 
-#                       header=TRUE, stringsAsFactors=FALSE)
-# byUniProtAccession = compare(set1= BeckGeneU, set2= GaoGeneU, compareColumn=1)
-# writeParts(compareList = byUniProtAccession, set1.desc = "Beck", set2.desc= "Gao")
-
-
 ## BOVINE ##
-normUp = read.table("~/Work/1_Milk/DilutionEffect/DE-Genes/Bovine-FINAL/Bt_norm_upall.txt", 
+bt.normUp = read.table("~/Work/1_Milk/DilutionEffect/DE-Genes/Bovine-FINAL/Bt_norm_upall.txt", 
                         header=TRUE, stringsAsFactors=FALSE)
-adjUp = read.table("~/Work/1_Milk/DilutionEffect/DE-Genes/Bovine-FINAL/Bt_adj_upall.txt", 
+bt.adjUp = read.table("~/Work/1_Milk/DilutionEffect/DE-Genes/Bovine-FINAL/Bt_adj_upall.txt", 
                        header=TRUE, stringsAsFactors=FALSE)
 
-byRefSeq = compare(set1= normUp, set2= adjUp, compareColumn=1)
+bt.byRefSeq = compare(set1= bt.normUp, set2= bt.adjUp, compareColumn=1)
 
-writeParts(compareList = byRefSeq, set1.desc = "upNorm", set2.desc= "upAdj")
+writeParts(compareList = bt.byRefSeq, set1.desc = "upNorm", set2.desc= "upAdj")
+
+
+
+## HUMAN ##
+hs.normUp = read.table("~/Work/1_Milk/DilutionEffect/DE-Genes/Human-FINAL/Hs_norm_upall.txt", 
+                    header=TRUE, stringsAsFactors=FALSE)
+hs.adjUp = read.table("~/Work/1_Milk/DilutionEffect/DE-Genes/Human-FINAL/Hs_adj_upall.txt", 
+                   header=TRUE, stringsAsFactors=FALSE)
+
+hs.byRefSeq = compare(set1= hs.normUp, set2= hs.adjUp, compareColumn=1)
+
+writeParts(compareList = hs.byRefSeq, set1.desc = "upNorm", set2.desc= "upAdj")
