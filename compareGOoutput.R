2f555c0acb7629e7d4a434184dc08e8818acb364
KristenBeck <kristenbeck527@gmail.com>
2014-01-06 14:51:50 -0800

    renamed directory to work better with cloned github repo for manuscript

--- compareGOoutput.R
+++
@@ -1,8 +0,0 @@
-# compareGOoutput.R
-
-setwd("Work/1_Milk/DilutionEffect/DAVID-GO-Output/Human-DefaultGOforswitchers/")
-
-switchers = read.delim("upAdj_unique-GOdefaults.txt", header=TRUE, sep = "\t", na.string="NA")
-unadjUp   = read.delim("upNorm-GOdefaults.txt", header=TRUE, sep = "\t", na.string="NA")
-
-length(intersect(switchers$Term, unadjUp$Term))
