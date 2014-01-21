2e295af1f0ce756894fcbfd9da05f16df0514aa9
KristenBeck <kristenbeck527@gmail.com>
2014-01-21 15:33:54 -0800

    Reorganized for functions to be at top and cleaned up threshold determination.

--- thresholdDetermination.R
+++ thresholdDetermination.R
@@ -3,65 +3,14 @@
 # PhD Candidate, UC Davis Genome Center
 # www.korflab.ucdavis.edu
 
-
-## Objectives: ---------------------
+# # # # # # 
+# 
+# OBJECTIVES
+#
+# # # # # # 
 # 1. Determine if dilution effect adjustment is necessary based on Kolmogorov-Smirnov and Wilcox test
 # 2. If so, calculate an appropriate threshold for high abundance genes
 # Note: The threshold determined for each replicate and subsequently used as input for dilutioneffect.pl
-
-## 0. Load expression data ---------------------
-# Check to see if files are declared on command line or console
-if (exists("file1") & exists("file2")) {
-    cat("File names specified in console.\n")
-} else {
-    args = commandArgs(trailingOnly=TRUE)
-    if(length(args) < 2) stop("Usage: Files undefined, please specify expression data. 
-    If working from command line use
-          $ Rscript thresholdDetermination.R <file1.txt> <file2.txt>
-    If working in Rstudio console use
-          > file1 = \"<file1.txt>\"
-          > file2 = \"<file2.txt>\"" )
-    file1 = args[1]
-    file2 = args[2]
-}
-cat(  "Expression data 1 specified: ", file1,
-      "\nExpression data 2 specified: ", file2, "\n")
-
-expr1 = read.table(file1, stringsAsFactors=FALSE, header=TRUE)
-expr2 = read.table(file2, stringsAsFactors=FALSE, header=TRUE)
-
-###
-### TODO - make a third args and use that to allow people to set their own quantile down stream
-###
-
-
-## 1. How similar are the distributions of two expression data sets? ---------------------
-# Determine the difference between distributions of both samples using statistical tests
-# If p < 0.05 in both Wilcoxon and KS test then you can confidently reject the null hypothesis and 
-# conclude that the distributions are different enough to warrant a dilution adjustment
-
-
-# Mann-Whitney-Wilcoxon Test
-w = wilcox.test(x=expr1$mean, y=expr2$mean)
-# Null hypothesis: expression data from stage 1 and stage 2 are identical populations
-# if p < 0.05 we can reject the null hypothesis, i.e. the two samples are from nonidentical distributions
-# ref: http://www.r-tutor.com/elementary-statistics/non-parametric-methods/mann-whitney-wilcoxon-test
-
-# Kolmogorov-Smirnov Test
-k = ks.test(x=expr1$mean,     y=expr2$mean)
-# interpret p-value as above i.e. p-value < 0.05 means the two samples are from nonidentical distributions
-# D = K-S test statistic aka the maximum difference between the x & y cumulative distribution function, higher value means larger difference
-
-# Test if distributions are different
-if (k$p.value < 0.05 & w$p.value < 0.05) {
-    cat("Distributions are statistically different.\nProceeding to calculate threshold.\n")
-} else {
-    warning("Distributions are not statistically different and dilution adjustment is not required.\nWilcoxon Test p-value = ", w$p.value,
-        "\nKS Test p-value = ", k$p.value)
-    stop("Exiting script")
-}
-
-
 
 
 # # # # # # 
@@ -89,7 +38,7 @@ determineThreshold =
         # Estimate the dilution adjustment
         aboveQuantile = sum(tmp[tmp > qq]) 
         adjFactor = 1/(1-(aboveQuantile/totalExpr))
-        cat("The estimated diution adjustment is", adjFactor, "\n")
+        cat("The estimated dilution adjustment is", adjFactor, "\n")
         
         cat("The quantile determined threshold is", threshold, "\n")
         
@@ -97,25 +46,58 @@ determineThreshold =
     }
 
 
-# # # # # # 
-# 
-# THRESHOLDS
-#
-# # # # # # 
+## 0. Load expression data ---------------------
+# Check to see if files are declared on command line or console
+if (exists("file1") & exists("file2")) {
+    cat("File names specified in console.\n")
+} else {
+    args = commandArgs(trailingOnly=TRUE)
+    if(length(args) < 2) stop("Usage: Files undefined, please specify expression data. 
+    If working from command line use
+          $ Rscript thresholdDetermination.R <file1.txt> <file2.txt>
+    If working in Rstudio console use
+          > file1 = \"<file1.txt>\"
+          > file2 = \"<file2.txt>\"" )
+    file1 = args[1]
+    file2 = args[2]
+}
 
-#### Step 2: What are the quantile determined thresholds? ####
-## Determine thresholds for both cow and human data at each lactation stage
-# Cow
-threshHarhayPrepub = sapply(2:7, function(x) determineThreshold(prepuberty, column = x, q= 0.9995))
-threshHarhay = sapply(2:7, function(x) determineThreshold(lactation, column = x, q= 0.9995))
+cat(  "Expression data 1 specified: ", file1,
+    "\nExpression data 2 specified: ", file2, "\n")
+expr1 = read.table(file1, stringsAsFactors=FALSE, header=TRUE)
+expr2 = read.table(file2, stringsAsFactors=FALSE, header=TRUE)
 
-# Human
-threshNRcolostrum = sapply(2:3, function(x) determineThreshold(NRcolostrum, column = x, q= 0.9995)) # dilution adjustment ~1.21
-threshNRtransitional = sapply(2:5, function(x) determineThreshold(NRtransitional, column = x, q= 0.9995)) # dilution adjustment ~1.21
-threshNRmature = sapply(2:7, function(x) determineThreshold(NRmature, column = x, q= 0.9995))
 
-# These thresholds can be used as input for dilution_effect.pl for each replicate
+## 1. Determine if distributions of two transcriptomes are similar ---------------------
+# Determine the difference between distributions of both samples using statistical tests
+# If p < 0.05 in both Wilcoxon and KS test then you can confidently reject the null hypothesis and 
+# conclude that the distributions are different enough to warrant a dilution adjustment
 
+# Mann-Whitney-Wilcoxon Test
+w = wilcox.test(x=expr1$mean, y=expr2$mean)
+# Null hypothesis: expression data from stage 1 and stage 2 are identical populations
+# if p < 0.05 we can reject the null hypothesis, i.e. the two samples are from nonidentical distributions
+# ref: http://www.r-tutor.com/elementary-statistics/non-parametric-methods/mann-whitney-wilcoxon-test
+
+# Kolmogorov-Smirnov Test
+k = ks.test(x=expr1$mean,     y=expr2$mean)
+# interpret p-value as above i.e. p-value < 0.05 means the two samples are from nonidentical distributions
+# D = K-S test statistic aka the maximum difference between the x & y cumulative distribution function, higher value means larger difference
+
+# Check if distributions are different and exit if they are not statistically different
+if (k$p.value < 0.05 & w$p.value < 0.05) {
+    cat("Distributions are statistically different.\nProceeding to calculate threshold.\n")
+} else {
+    warning("Distributions are not statistically different and dilution adjustment is not required.\nWilcoxon Test p-value = ", w$p.value,
+        "\nKS Test p-value = ", k$p.value)
+    stop("Exiting script")
+}
+
+
+## 1. Determine thresholds using quantile for top expression ---------------------
+threshExpr1 = sapply(2:ncol(expr1), function(x) determineThreshold(expr1, column = x, q= 0.9995))
+threshExpr2 = sapply(2:ncol(expr2), function(x) determineThreshold(expr2, column = x, q= 0.9995))
+# These thresholds are input for dilution_effect.pl and correspond to each replicate
 
 
 # # # # # # 
