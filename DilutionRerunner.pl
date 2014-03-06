900c13c73b9661d0511eb8db7b8d4a28e15e9f4a
KristenBeck <kristenbeck527@gmail.com>
2014-03-06 10:24:03 -0800

    Updated file handling

--- DilutionRerunner.pl
+++ DilutionRerunner.pl
@@ -50,29 +50,30 @@ close $threshFile;
 #}
 
 
-__END__
-
 # then everything else should work
-
 ## Harhay lactating cow RNA-seq count data ##
-my $reps = 6; # replicates
+## my $reps = 6; # replicates
 
 # update this
-my $path = "~/Work/1_Milk/DilutionEffect/RNASeqReps-Harhay"; # path to directory of data files
+## my $path = "~/Work/1_Milk/DilutionEffect/RNASeqReps-Harhay"; # path to directory of data files
 
-# update this
-my $fileroot = "lcountsRep"; # beginning of file without replicate number or extension
+
+## my $fileroot = "lcountsRep"; # beginning of file without replicate number or extension
 # thresholds returned from thresholdDetermination.R
 
 # this will be from above
-my @thresh = ('0.001734695', '0.001785946', '0.001719321', '0.001771564', '0.001733576', '0.001736869');
-run_dilution($reps, $path, $fileroot, @thresh);
+
+run_dilution($reps, $exprFileRoot, @thresh);
 
 
 
 
+# notes need to fix file names in sub routine
 
+# passing directory/file
+# but adj file needs it to be just file
 
+# may have to go back to the original way with path and file name specified
 
 # # # # # # # # # # #
 #
@@ -81,66 +82,29 @@ run_dilution($reps, $path, $fileroot, @thresh);
 # # # # # # # # # # #
 
 sub run_dilution {
-	my ($reps, $path, $fileroot, @thresh) = @_;
+	my ($reps, $exprFileRoot, @thresh) = @_;
 
 	for (my $i = 1; $i <= $reps; $i++) {
-		my $adj_file = "dilOUT_$fileroot$i.txt";
+
+		my $adj_file = join("_", "$exprFileRoot$i", "dilutionOUT.txt"); 
+		`./dilution_effect.pl -a $thresh[$i-1] $exprFileRoot$i.txt > $adj_file`; # everything ok to here
+
+
+		my $pseuct_file = join("_", "$exprFileRoot$i", "dilutionOUT_pseudoct.txt");
+		`./pseudocounter.pl $adj_file > $pseuct_file`; 
 		
-		`dilution_effect.pl -a $thresh[$i-1] $path/$fileroot$i.txt > $adj_file`;
-		`pseudocounter.pl $adj_file > pseuct_$adj_file`;
+		my $final_root = "$exprFileRoot$i";
+		$final_root =~ s/Data/Results/;
+		my $normEnding = "_norm.txt";
+		my $adjEnding  = "_adj.txt";
+				
 		
-		my $final_file = "$fileroot$i.txt";
-		
-		`sort pseuct_$adj_file | cut -f 1,2 > norm_$final_file`;
-		`sort pseuct_$adj_file | cut -f 1,3 > adj_$final_file`;
+		`sort $pseuct_file | cut -f 1,2 > $final_root$normEnding`;
+		`sort $pseuct_file | cut -f 1,3 > $final_root$adjEnding`;
 		
 		`rm $adj_file`;
-		`rm pseuct_$adj_file`;
+		`rm $pseuct_file`;
 	}
 
 }
-
-
-
-
-
-__DATA__
-Empirical Threshhold	0.009237
-
-__END__
-# #OLD CODE untouched
-# 
-# for (my $i = 1; $i <= 6; $i++) {
-# 	`dilution_effect.pl -a 0.009237 FullSet/lcountsRep$i.txt > FullSet/Adj_lcountsRep$i.txt`;
-# 	my $adj_file = "Adj_lcountsRep$i.txt";
-# 	`pseudocounter.pl FullSet/$adj_file > FullSet/pseuct_$adj_file`;
-# }
-# 
-# # run the dilution adjusted script on pre-puberty samples w empirical threshold
-# for (my $i = 1; $i <= 10; $i++) {
-# 	
-# 	`dilution_effect.pl -a 0.009237 FullSet/pcountsRep$i.txt > FullSet/Adj_pcountsRep$i.txt`;
-# 	# there should be no pcounts that can be adjusted, so use line below
-# 	`pseudocounter.pl FullSet/pcountsRep$i.txt > FullSet/pseuct_pcountsRep$i.txt`;
-# 	
-# 	# if there are, double check data and use line below to pseudoct
-# 	# my $adj_file = "Adj_pcountsRep$i.txt"; # these files should be empty
-# 	# `pseudocounter.pl FullSet/$adj_file > FullSet/pseuct_$adj_file`;
-# }
-# 
-# # sort check
-
-#OLD CODE untouched
-
-
-
-
-## Nommsen-Rivers mature lactating RNA-seq FPKM data ##
-$reps = 6; # replicates
-$path = "~/Work/1_Milk/DilutionEffect/RNASeqReps-NR";
-$fileroot = "MatureRep"; # beginning of file without replicate number or extension
-# thresholds returned from thresholdDetermination.R
-@thresh = ('0.001588403', '0.001685752', '0.001579439', '0.001796879', '0.001846405', '0.001697971');
-run_dilution($reps, $path, $fileroot, @thresh);
-
 
